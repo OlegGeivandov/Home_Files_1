7d3aea3abfaaa25baf09f5a4a3cc17ab0fd9c743
OlegGeivandov <140657755+OlegGeivandov@users.noreply.github.com>
2023-08-02 01:01:40 +0300

    работает

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -16,20 +18,28 @@ import java.util.Scanner;
 //
 //        4) Прочитать файл b2.txt и вывести из него такие строки, каких нет в файле b1.txt
 
-// Тест Тест Тест
+
 public class Main {
     public static void main(String[] args) {
 
-        String fileInput =  "src/files/a1.txt";
+        String fileInput1 =  "src/files/a1.txt";
+        String fileInput2 =  "src/files/a2.txt";
+        String fileInput3 =  "src/files/b1.txt";
+        String fileInput4 =  "src/files/b2.txt";
+
         String fileOutput = "src/files/res.txt";
 
-        readIntFromFile(fileInput, fileOutput);
+        readIntFromFileAndWriteSumToFile(fileInput1, fileOutput);
 
+        readArrayFromFileAndWriteAverageToFile(fileInput2, fileOutput);
 
+        readStringFromFileAndWriteToFile(fileInput3, fileOutput);
+
+        readStringFromFileAndWriteToFile1(fileInput3, fileInput4, fileOutput);
 
     }
 
-    static void readIntFromFile(String fileInput, String fileOutput){
+    static void readIntFromFileAndWriteSumToFile(String fileInput, String fileOutput){
         try{
             Scanner sc = new Scanner(new File(fileInput));
             int x = sc.nextInt();
@@ -49,5 +59,98 @@ public class Main {
         }
     }
 
+    static void readArrayFromFileAndWriteAverageToFile(String fileInput, String fileOutput){
+        try{
+            Scanner sc = new Scanner(new File(fileInput));
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            double avg = 0;
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+                avg+=arr[i];
+            }
+            avg/=n;
+            FileWriter output = new FileWriter((new File(fileOutput)));
+            System.out.println("среднее арифметическое " + avg);
+            String s = "среднее арифметическое " + avg;
+            output.write(s);
+            sc.close();
+            output.close();
+        }
+        catch(FileNotFoundException r){
+            System.out.println("нет файла");
+        }
+        catch(IOException e){
+            System.out.println("нет файла");
+        }
+    }
+
+    static void readStringFromFileAndWriteToFile(String fileInput, String fileOutput){
+        try{
+            Scanner sc = new Scanner(new File(fileInput));
+            String s;
+            ArrayList<String> list = new ArrayList<String>();
+            while (sc.hasNextLine()) {
+                s = sc.nextLine();
+                list.add(s);
+            }
+
+            FileWriter output = new FileWriter((new File(fileOutput)));
+                for (int i = list.size()-1; i >= 0; i--) {
+//                    System.out.println(Arrays.toString(list.toArray()));
+                    String s1 = list.get(i);
+                    System.out.println(s1);
+                    output.write(s1 + "\n");
+                }
+//            for (String s2 : list) {System.out.println(s2);}
+            sc.close();
+            output.close();
+        }
+        catch(FileNotFoundException r){
+            System.out.println("нет файла");
+        }
+        catch(IOException e){
+            System.out.println("нет файла");
+        }
+    }
+
+
+    static void readStringFromFileAndWriteToFile1(String fileInput1, String fileInput2,  String fileOutput){
+        try{
+            Scanner sc = new Scanner(new File(fileInput1));
+            String s;
+            ArrayList<String> list1 = new ArrayList<String>();
+            while (sc.hasNextLine()) {
+                s = sc.nextLine();
+                list1.add(s);
+            }
+
+            sc = new Scanner(new File(fileInput2));
+            ArrayList<String> list2 = new ArrayList<String>();
+            while (sc.hasNextLine()) {
+                s = sc.nextLine();
+                list2.add(s);
+            }
+
+            ArrayList<String> listResult = new ArrayList<String>();
+
+            FileWriter output = new FileWriter((new File(fileOutput)));
+            for (int i = 0; i < list2.size(); i++) {
+                if(!list1.contains(list2.get(i))) {
+                    String s1 = list2.get(i);
+                    System.out.println(s1);
+                    output.write(s1 + "\n");
+                }
+            }
+            sc.close();
+            output.close();
+        }
+        catch(FileNotFoundException r){
+            System.out.println("нет файла");
+        }
+        catch(IOException e){
+            System.out.println("нет файла");
+        }
+    }
 
 }
